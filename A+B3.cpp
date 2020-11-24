b8ed095d7fcb1195b427b86cd8eff3be9474a65f
yu123ge <74582350+yu123ge@users.noreply.github.com>
2020-11-24 19:09:28 +0800

    a+b

---
+++ A+B3.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int a,b;
+	while(cin>>a>>b){
+		int s;
+		s=a+b;	cout<<s<<"\n"<<endl;
+		
+	} 
+
+	
+	
+	return 0;
+}
---
+++ A+B4.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int a,n,s;
+	while(cin>>n&&n){
+		for(int i=1;i<=n;i++){
+		cin>>a;
+		s+=a;
+	}
+	cout<<s<<endl;
+	s=0;
+}
+	return 0;
+}
