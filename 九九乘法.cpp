907483f890aced17121cfe86119c95214aecc6a9
yu123ge <74582350+yu123ge@users.noreply.github.com>
2022-01-21 15:00:55 +0800

    Add files via upload

---
+++ 九九乘法.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int i,j,x,y;
+	x=1;
+	for(i=1;i<10;i++){
+		y=1;
+		for(j=0;j<x;j++){		
+		cout<<x<<"*"<<y<<"="<<x*y<<" "; 
+		y++;	
+			}
+	cout<<endl;	
+	x++;
+    
+	}
+	return 0; 
+} 
